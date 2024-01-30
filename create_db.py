ba9fdaf201b6279b667aec48dbb2601a70272f59
Scynes <dustin.greyfield@gmail.com>
2024-01-30 14:22:00 -0500

    add: user profile and game genrea table generation

--- create_db.py
+++ create_db.py
@@ -1,1 +1,48 @@
-print('hello')
+import sqlite3
+
+from faker import Faker
+
+faker_instance = Faker()
+
+# Create the mock i/o connect to write output.
+connection = sqlite3.connect('mock_data.db')
+
+# Pencile the data to the connection.
+cursor = connection.cursor()
+
+# Primary user profile table - contains fields relevant to the user.
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS user_profiles (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT,
+        email TEXT,
+        age INTEGER,
+        zip_code INTEGER,
+        zip_code_radius INTEGER,
+        gaming_genre_id INTEGER,
+        FOREIGN KEY (gaming_genre_id) REFERENCES gaming_genres(id)
+    )
+''')
+
+# FK table that represents the genres available.
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS gaming_genres (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    )
+''')
+
+# Mock gaming genres not available with Faker.
+gaming_genres = ['RPG', 'Fantasy', 'FPS', 'SciFi', 'Strategy', 'Puzzle']
+
+for genre in gaming_genres:
+    cursor.execute('''
+        INSERT INTO gaming_genres (name)
+        VALUES (?)
+    ''', (genre,))
+
+# Commit the DB changes.
+connection.commit()
+
+# Close the connection.
+connection.close()
